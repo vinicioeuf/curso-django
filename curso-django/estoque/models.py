5e466ecc2bade4e904e06831a47fcf922a2a164e
Vinicio Eufrazio <59233562+vinicioeuf@users.noreply.github.com>
2024-12-30 12:34:22 -0300

    alguns problems

--- curso-django/estoque/models.py
+++ curso-django/estoque/models.py
@@ -1,7 +1,40 @@
 from django.db import models
+from django.forms import ValidationError
 
-# Create your models here.
+class Categoria(models.Model):
+    id_categoria = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=250)
+    descricao = models.CharField(max_length=255)
+
 class Produto(models.Model):
+    id_produto = models.AutoField(primary_key=True, default=0)
     nome = models.CharField(max_length=100)
-    quantidade =models.IntegerField()
+    quantidade = models.IntegerField()
     preco = models.DecimalField(max_digits=10, decimal_places=2)
+    categoria = models.ForeignKey(
+        'estoque.Categoria', 
+        on_delete=models.CASCADE, 
+        null=True,  # Permite que o campo seja nulo
+        blank=True  # Permite que o campo seja deixado em branco em formulários
+    )
+    descricao = models.TextField(default="")
+
+
+
+class MovimentacaoEstoque(models.Model):
+    id_movimentacao = models.AutoField(primary_key=True, default=0)
+    produto = models.ForeignKey('estoque.Produto', on_delete=models.CASCADE)
+    tipo = models.CharField(max_length=20, choices=[('entrada', 'Entrada'), ('saida', 'Saída')])
+    quantidade = models.IntegerField()
+    data_movimentacao = models.DateTimeField()
+    observacoes = models.TextField(blank=True, null=True)
+    def clean(self):
+        if self.quantidade <= 0:
+            raise ValidationError("A quantidade deve ser maior que zero.")
+
+class Fornecedor(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100)
+    telefone = models.CharField(max_length=15)
+    email = models.EmailField(max_length=255)
+    endereco = models.CharField(max_length=200)
--- curso-django/estoque/views.py
+++ curso-django/estoque/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
 
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, logout
+from django.contrib.auth import login as loginDjango
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def cadastro(request):
+    if request.method == 'GET':
+        return render(request, 'autenticacao/cadastro.html')
+    else:
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        user = User.objects.filter(email=email).first()
+        if user:
+            return HttpResponse('Já existe um usuário com este e-mail.')
+       
+        novo_usuario = User.objects.create_user(email=email, password=senha, first_name=nome, username=email)
+        novo_usuario.save()
+
+        return redirect('/login')
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'autenticacao/login.html')
+    else:
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        user = authenticate(username=email, password=senha)
+
+        if user:
+            loginDjango(request, user)
+            return redirect('autenticacao/index')
+        else:
+            return HttpResponse('E-mail ou senha inválidas')
+        
+def logout_view(request):
+    logout(request)
+
+
+
