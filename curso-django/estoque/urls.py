d69674cc1c5c38f42deffdb4c72a298357826b9f
Vinicio Eufrazio <59233562+vinicioeuf@users.noreply.github.com>
2024-12-31 23:49:26 -0300

    crud de todas as tabelas (não testei se der merda só vou ajeitar ano que vem)

--- curso-django/estoque/urls.py
+++ curso-django/estoque/urls.py
@@ -15,8 +15,24 @@ urlpatterns = [
     path('categorias/deletar/<int:id>/', views.deletar_categoria, name='deletar_categoria'),
     path('categorias/', views.listar_categorias, name='listar_categorias'),
     path('produtos/', views.listar_produtos, name='listar_produtos'),
+    
     path('produtos/adicionar/', views.adicionar_produto, name='adicionar_produto'),
-    # path('produtos/', views.listar_produtos, name='listar_produtos'),
+    path('produtos/editar/<int:id>/', views.editar_produto, name='editar_produto'),
+    path('produtos/deletar/<int:id>/', views.deletar_produto, name='deletar_produto'),
+    
+    # URLs para Movimentação de Estoque
+    path('movimentacoes/', views.listar_movimentacoes, name='listar_movimentacoes'),
+    path('movimentacoes/adicionar/', views.adicionar_movimentacao, name='adicionar_movimentacao'),
+    path('movimentacoes/editar/<int:id>/', views.editar_movimentacao, name='editar_movimentacao'),
+    path('movimentacoes/deletar/<int:id>/', views.deletar_movimentacao, name='deletar_movimentacao'),
+
+    # URLs para Fornecedor
+    path('fornecedor/', views.listar_fornecedores, name='listar_fornecedores'),
+    path('fornecedor/adicionar/', views.adicionar_fornecedor, name='adicionar_fornecedor'),
+    path('fornecedor/editar/<int:id>/', views.editar_fornecedor, name='editar_fornecedor'),
+    path('fornecedor/deletar/<int:id>/', views.deletar_fornecedor, name='deletar_fornecedor'),
+    
+    #URLs resetar senha
     path('reset_password/', auth_views.PasswordResetView.as_view(), name="reset_password"),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
     path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
--- curso-django/estoque/views.py
+++ curso-django/estoque/views.py
@@ -1,14 +1,15 @@
 
+from datetime import timezone
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, logout
 from django.contrib.auth import login as loginDjango
 from django.contrib.auth.decorators import login_required
-from .models import Produto, Categoria
+from .models import Produto, Categoria, Fornecedor, MovimentacaoEstoque
 
 def index(request):
-    produtos = Produto.objects.all()  # Busca todos os registros da tabela Produto
+    produtos = Produto.objects.all() 
     return render(request, 'index.html', {'produtos': produtos})
 
 @login_required(login_url="/login/")
@@ -23,13 +24,13 @@ def adicionar_produto(request):
     pass
 
 
-
+#CRUD CATEGORIA ==================================================================================
 def adicionar_categoria(request):
     if request.method == 'POST':
         nome = request.POST.get('nome')
         descricao = request.POST.get('descricao')
         Categoria.objects.create(nome=nome, descricao=descricao)
-        return redirect('index')  # Redirecionar para a lista de categorias
+        return redirect('index') 
     return render(request, 'categoria/adicionar_categoria.html')
 
 def listar_categorias(request):
@@ -52,6 +53,8 @@ def deletar_categoria(request, id):
         return redirect('listar_categorias')
     return render(request, 'deletar_categoria.html', {'categoria': categoria})
 
+
+#CRUD PRODUTO ==================================================================================
 def adicionar_produto(request):
     categorias = Categoria.objects.all()
     if request.method == 'POST':
@@ -59,7 +62,7 @@ def adicionar_produto(request):
         preco = request.POST.get('preco')
         quantidade = request.POST.get('quantidade')
         descricao = request.POST.get('descricao')
-        id_categoria = request.POST.get('id_categoria')  # Certifique-se de que o nome seja consistente
+        id_categoria = request.POST.get('id_categoria')  
         categoria = Categoria.objects.get(id_categoria=id_categoria)
         
         Produto.objects.create(
@@ -75,6 +78,117 @@ def adicionar_produto(request):
 def listar_produtos(request):
     produtos = Produto.objects.select_related('categoria').all()
     return render(request, 'produto/listar_produtos.html', {'produtos': produtos})
+
+def editar_produto(request, id):
+    produto = Produto.objects.get(id=id)
+    if request.method == 'POST':
+        produto.nome = request.POST.get('nome')
+        produto.preco = request.POST.get('preco')
+        produto.descricao = request.POST.get('descricao')
+        categoria_id = request.POST.get('categoria_id')
+        produto.categoria_id = Categoria.objects.get(id_categoria=categoria_id)
+        produto.save()
+        return redirect('index')
+    return render(request, 'editar_produto.html', {'produto': produto})
+
+def deletar_produto(request, id):
+    produto = Produto.objects.get(id=id)
+    if request.method == 'POST':
+        produto.delete()
+        return redirect('index')
+    return render(request, 'deletar_produto.html', {'produto': produto})
+
+#CRUD FORNECEDOR ==================================================================================
+def adicionar_fornecedor(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        telefone = request.POST.get('telefone')
+        email = request.POST.get('email')
+        endereco = request.POST.get('endereco')
+
+        Fornecedor.objects.create(
+            nome=nome,
+            telefone=telefone,
+            email=email,
+            endereco=endereco
+        )
+        return redirect('listar_fornecedores')
+    return render(request, 'fornecedor/adicionar_fornecedor.html')
+
+def listar_fornecedores(request):
+    fornecedores = Fornecedor.objects.all()
+    return render(request, 'fornecedor/listar_fornecedores.html', {'fornecedores': fornecedores})
+
+def editar_fornecedor(request, id):
+    fornecedor = Fornecedor.objects.get(id=id)
+    if request.method == 'POST':
+        fornecedor.nome = request.POST.get('nome')
+        fornecedor.telefone = request.POST.get('telefone')
+        fornecedor.email = request.POST.get('email')
+        fornecedor.endereco = request.POST.get('endereco')
+        fornecedor.save()
+        return redirect('listar_fornecedores')
+    return render(request, 'fornecedor/editar_fornecedor.html', {'fornecedor': fornecedor})
+
+def deletar_fornecedor(request, id):
+    fornecedor = Fornecedor.objects.get(id=id)
+    if request.method == 'POST':
+        fornecedor.delete()
+        return redirect('listar_fornecedores')
+    return render(request, 'fornecedor/deletar_fornecedor.html', {'fornecedor': fornecedor})
+
+#CRUD MOVIMENTAÇÃO DO ESTOQUE ==================================================================================
+def adicionar_movimentacao(request):
+    produtos = Produto.objects.all()
+    if request.method == 'POST':
+        produto_id = request.POST.get('produto')
+        tipo = request.POST.get('tipo')
+        quantidade = int(request.POST.get('quantidade'))
+        data_movimentacao = timezone.now()
+        observacoes = request.POST.get('observacoes')
+
+        
+        if quantidade <= 0:
+            return render(request, 'movimentacaoestoque/adicionar_movimentacao.html', {
+                'produtos': produtos,
+                'error': "A quantidade deve ser maior que zero."
+            })
+
+        MovimentacaoEstoque.objects.create(
+            produto_id=produto_id,
+            tipo=tipo,
+            quantidade=quantidade,
+            data_movimentacao=data_movimentacao,
+            observacoes=observacoes
+        )
+        return redirect('listar_movimentacoes')
+    return render(request, 'movimentacaoestoque/adicionar_movimentacao.html', {'produtos': produtos})
+
+def listar_movimentacoes(request):
+    movimentacoes = MovimentacaoEstoque.objects.select_related('produto').all()
+    return render(request, 'movimentacaoestoque/listar_movimentacoes.html', {'movimentacoes': movimentacoes})
+
+def editar_movimentacao(request, id):
+    movimentacao = MovimentacaoEstoque.objects.get(id_movimentacao=id)
+    produtos = Produto.objects.all()
+    if request.method == 'POST':
+        movimentacao.produto_id = request.POST.get('produto')
+        movimentacao.tipo = request.POST.get('tipo')
+        movimentacao.quantidade = int(request.POST.get('quantidade'))
+        movimentacao.observacoes = request.POST.get('observacoes')
+        movimentacao.save()
+        return redirect('listar_movimentacoes')
+    return render(request, 'movimentacaoestoque/editar_movimentacao.html', {
+        'movimentacao': movimentacao,
+        'produtos': produtos
+    })
+
+def deletar_movimentacao(request, id):
+    movimentacao = MovimentacaoEstoque.objects.get(id_movimentacao=id)
+    if request.method == 'POST':
+        movimentacao.delete()
+        return redirect('listar_movimentacoes')
+    return render(request, 'movimentacaoestoque/deletar_movimentacao.html', {'movimentacao': movimentacao})
 
 
 def cadastro(request):
